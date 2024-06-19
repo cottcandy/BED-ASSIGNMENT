8d76ef87bb9815327895c41012ab8aaa4cdcd277
cottcandy <s10258514@connect.np.edu.sg>
2024-06-19 14:53:12 +0800

    updated sql

--- sql/createTables.sql
+++ sql/createTables.sql
@@ -1,60 +1,116 @@
+DROP TABLE IF EXISTS Donations;
+DROP TABLE IF EXISTS Volunteers;
+DROP TABLE IF EXISTS Posts;
+DROP TABLE IF EXISTS Events;
+DROP TABLE IF EXISTS Administrators;
+DROP TABLE IF EXISTS Members;
+
+
+
 CREATE TABLE Members (
-  member_id INT IDENTITY(1,1) PRIMARY KEY,
-  member_email VARCHAR(255) NOT NULL UNIQUE, 
-  first_name VARCHAR(50) NOT NULL,
-  last_name VARCHAR(50) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  birthday DATE,
-  phone_number VARCHAR(20)
+	MemberID		INT IDENTITY(1, 1),
+    NewMemberID AS 'M' + RIGHT('000' + CAST(MemberID AS VARCHAR(3)), 3) PERSISTED,
+	MemberEmail		VARCHAR(255)	NOT NULL UNIQUE, 
+	FirstName		VARCHAR(50)		NOT NULL,
+	LastName		VARCHAR(50)		NOT NULL,
+	MemberPassword	CHAR(12)		NOT NULL UNIQUE,
+	Birthday		DATE			NOT NULL,
+	PhoneNumber		VARCHAR(20)		NOT NULL UNIQUE,
+	CONSTRAINT PK_Members PRIMARY KEY (NewMemberID)
 );
 
 
 CREATE TABLE Administrators (
-  administrator_id INT IDENTITY(1,1) PRIMARY KEY,
-  administrator_email VARCHAR(255) NOT NULL UNIQUE,
-  password VARCHAR(255) NOT NULL 
+	AdminID			CHAR(4)			CHECK (AdminID LIKE 'A%'),
+	AdminEmail		VARCHAR(255)	NOT NULL UNIQUE,
+	AdminPassword	CHAR(12)		NOT NULL UNIQUE,
+	CONSTRAINT PK_Administrators PRIMARY KEY (AdminID)
+);
+
+
+CREATE TABLE Events (
+	EventID		INT IDENTITY(1, 1),
+    EventType	VARCHAR(50)			NOT NULL,
+	EventDate	DATE				NOT NULL,
+	EventTime	DATETIME			NOT NULL,
+    NewEventID	AS ( 
+        CASE 
+            WHEN EventType = 'Healthcare' THEN 'H' + RIGHT('000' + CAST(EventID AS VARCHAR(3)), 3)
+            WHEN EventType = 'CSR' THEN 'C' + RIGHT('000' + CAST(EventID AS VARCHAR(3)), 3)
+            WHEN EventType = 'Environment' THEN 'E' + RIGHT('000' + CAST(EventID AS VARCHAR(3)), 3)
+            WHEN EventType = 'Education' THEN 'ED' + RIGHT('000' + CAST(EventID AS VARCHAR(3)), 3)
+        END )
+		PERSISTED,
+	AdminID		CHAR(4),
+	CONSTRAINT PK_Events PRIMARY KEY (NewEventID, EventType, EventDate, EventTime),
+	CONSTRAINT FK_Events_AdminID FOREIGN KEY (AdminID) REFERENCES Administrators(AdminID)
 );
 
 
 CREATE TABLE Posts (
-  post_id INT IDENTITY(1,1) PRIMARY KEY,
-  administrator_id INT NOT NULL,
-  title VARCHAR(255) NOT NULL,
-  content TEXT,
-  picture_url VARCHAR(255),
-  event_type VARCHAR(50) NOT NULL,
-  created_at DATETIME DEFAULT GETDATE(),
-  FOREIGN KEY (administrator_id) REFERENCES administrators(administrator_id)
-);
-
-
-CREATE TABLE Donations ( 
-    donation_id INT IDENTITY(1,1) PRIMARY KEY, 
-    member_id INT, 
-    event_type VARCHAR (100), 
-    donation_amount DECIMAL(10, 2), 
-    donation_date DATE, 
-    FOREIGN KEY (member_id) REFERENCES Members(member_id) 
-); 
-
-
-CREATE TABLE PaymentDetails ( 
-    payment_id INT IDENTITY(1,1) PRIMARY KEY, 
-    donation_id INT, 
-    payment_method VARCHAR(50), 
-    payment_date DATE, 
-    payment_status VARCHAR(50), 
-    FOREIGN KEY (donation_id) REFERENCES Donations(donation_id) 
+	PostID		INT IDENTITY(1, 1),
+    NewPostID AS 'P' + RIGHT('000' + CAST(PostID AS VARCHAR(3)), 3) PERSISTED,
+	Title		VARCHAR(255)	NOT NULL,
+	Content		TEXT			NOT NULL,
+	PictureURL	VARCHAR(255)	NOT NULL,
+	PostedOn	DATETIME DEFAULT GETDATE(),
+	AdminID		CHAR(4),
+	NewEventID	VARCHAR(4),
+	EventType	VARCHAR(50),
+	EventDate   DATE,
+	EventTime	DATETIME,
+	CONSTRAINT PK_Posts PRIMARY KEY (PostID),
+	CONSTRAINT FK_Posts_AdminID FOREIGN KEY (AdminID) REFERENCES Administrators(AdminID),
+	CONSTRAINT FK_Posts_Event FOREIGN KEY (NewEventID, EventType, EventDate, EventTime) REFERENCES Events(NewEventID, EventType, EventDate, EventTime)
 );
 
 
 CREATE TABLE Volunteers (
-  voluteer_id INT IDENTITY(1,1) PRIMARY KEY,
-  post_id INT NOT NULL,
-  member_id INT NOT NULL,
-  member_email VARCHAR(255) NOT NULL,
-  event_type VARCHAR(50) NOT NULL,
-  available_date DATE NOT NULL,
-  FOREIGN KEY (post_id) REFERENCES posts(post_id),
-  FOREIGN KEY (member_id) REFERENCES members(member_id)
+	VolunteerID INT IDENTITY(1,1),
+	NewVolunteerID AS 'V' + RIGHT('000' + CAST(VolunteerID AS VARCHAR(3)), 3) PERSISTED,
+	NewEventID		VARCHAR(4),
+	EventType		VARCHAR(50),
+	EventDate		DATE,
+	EventTime		DATETIME,
+	NewMemberID		VARCHAR(4),
+	CONSTRAINT PK_Volunteers PRIMARY KEY (NewVolunteerID),
+	CONSTRAINT FK_Volunteers_Event FOREIGN KEY (NewEventID, EventType, EventDate, EventTime) REFERENCES Events(NewEventID, EventType, EventDate, EventTime),
+	CONSTRAINT FK_Volunteers_MemberID FOREIGN KEY (NewMemberID) REFERENCES Members(NewMemberID)
 );
+
+
+CREATE TABLE Donations ( 
+    DonationID		INT IDENTITY(1, 1),
+    NewDonationID AS 'D' + RIGHT('000' + CAST(DonationID AS VARCHAR(3)), 3) PERSISTED,  
+    DonationAmount	INT,
+    DonationDate	DATE, 
+	NewEventID		VARCHAR(4),
+	EventType		VARCHAR(50),
+	EventDate		DATE,
+	EventTime		DATETIME,
+	NewMemberID		VARCHAR(4),
+    CONSTRAINT PK_Donations PRIMARY KEY (DonationID), 
+	CONSTRAINT FK_Donations_Event FOREIGN KEY (NewEventID, EventType, EventDate, EventTime) REFERENCES Events(NewEventID, EventType, EventDate, EventTime),
+	CONSTRAINT FK_Donations_MemberID FOREIGN KEY (NewMemberID) REFERENCES Members(NewMemberID)
+); 
+
+/*
+INSERT INTO Members (MemberEmail, FirstName, LastName, MemberPassword, Birthday, PhoneNumber) VALUES
+    ('john123@email.com', 'John', 'Wong', 'johnwong123@', '12/03/2001', '98765432'),
+    ('brucelee@email.com', 'Bruce', 'Lee', 'bruceee@1234', '04/01/1998', '87654321');
+
+INSERT INTO Administrators (AdminID, AdminEmail, AdminPassword) VALUES
+	('A001', 'S12345678@connect.np.edu.sg', 'S12345678@!8');
+
+INSERT INTO Events (EventID, EventType, EventDate, EventTime) VALUES
+	();
+
+INSERT INTO Posts (PostID, Title, Content, PictureURL, PostedOn) VALUES
+	();
+
+INSERT INTO Volunteers (VolunteerID) VALUES
+	();
+
+INSERT INTO Donations (DonationID) VALUES
+	();
+*/
